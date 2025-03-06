30ea0a843916eedd509e99d8f7e41e298b464268
Phanindra Kumar <162738208+phanindrakumar08@users.noreply.github.com>
2025-03-06 17:14:15 -0500

    Delete customer_satisfaction directory

--- customer_satisfaction/steps/config.py
+++
@@ -1,8 +0,0 @@
-from zenml.models import BaseZenModel as BaseParameters
-
-class ModelNameConfig(BaseParameters):
-    """
-    Model Configs
-    """
-    model_name: str = "LinearRegression"
-    
